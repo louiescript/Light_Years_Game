3513c8a37cca22e0724c4f6d0309376785804ba6
EL <ernestlouis@outlook.com>
2025-01-21 18:40:41 -0600

    generated a 100X100 window

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <print>
 #include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
+//#include <SFML/Audio.hpp>
+//#include <SFML/System.hpp>
+//#include <SFML/Window.hpp>
+
 
 
 int main()
 {
+	
+	sf::VideoMode mode(sf::Vector2u(100,100));
+	sf::RenderWindow renderWindow{mode,"MyWindow"};
 
 	std::string new_name{ "timothy" };
 
