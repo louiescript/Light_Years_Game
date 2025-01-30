778539208af85ddfd6d1898bfdfd2cecb0d9522b
EL <ernestlouis@outlook.com>
2025-01-29 22:37:15 -0600

    add load world function

--- LightYearsEngine/include/framework/App.h
+++ LightYearsEngine/include/framework/App.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include "framework/Core.h"
 
 //says that everything created belongs to ly namespace, prevents name clashing
 namespace ly
 {
+	class World;
 	class App
 	{
 	public:
 		App(); //alt enter to create definition automatically
 		void Run();
 
+		template<typename WorldType>
+		weak<WorldType> LoadWorld();
 
 	private:
 		void TickInternal(float delta_time);
@@ -23,6 +27,15 @@ namespace ly
 		sf::RenderWindow m_window; //member varible window
 		float m_target_frame_rate;
 		sf::Clock m_tick_clock; //keeps a record of the timing
+
+		shared<World> current_world;
 	};
+
+	template<typename WorldType>
+	weak<WorldType> App::LoadWorld()
+	{
+		shared<WorldType> new_world{new WorldType{this}}
+	}
+
 
 }
